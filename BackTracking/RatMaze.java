d33eaca68960f42f5b9a4d5aa26cc69fcd45af4a
saprem <smsaprem@gmail.com>
2017-04-08 19:06:34 +0530

    Update RatMaze.java

--- BackTracking/RatMaze.java
+++ BackTracking/RatMaze.java
@@ -15,25 +15,21 @@ Else
 Code: 
 
 boolean isSafe(int maze[][], int x, int y) {
-		// if (x,y outside maze) return false
 		return (x >= 0 && x < N && y >= 0 && y < N && maze[x][y] == 0);
 }
 
 boolean solveMazeUtil(int maze[][], int x, int y, int sol[][]) {
-		// if (x,y is goal) return true
+		
 		if (x == N - 1 && y == N - 1) {
 			sol[x][y] = 1;
 			return true;
 		}
-		// Check if maze[x][y] is valid
-		if (isSafe(maze, x, y) == true) {
-			// mark x,y as part of solution path
-			sol[x][y] = 1;
 
+		if (isSafe(maze, x, y) == true) {
+			sol[x][y] = 1;
 			/* Move forward in x direction */
 			if (solveMazeUtil(maze, x + 1, y, sol))
 				return true;
-
 			/*
 			 * If moving in x direction doesn't give solution then Move down in
 			 * y direction
